9397a556b5f06a337ad87b7f4d1015cc8689a00f
aquist0295 <aquist@rocketmail.com>
2026-08-06 09:27:33 -0400

    adding types.hpp for variable aliases

---
+++ src/types.hpp
@@ -0,0 +1,10 @@
+#ifndef TYPES_HPP
+#define TYPES_HPP
+#include <cstdint>
+
+using u8  = std::uint8_t;
+using u16 = std::uint16_t;
+using u32 = std::uint32_t;
+using u64 = std::uint64_t;
+
+#endif // TYPES_HPP
